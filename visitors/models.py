74b7d6ca03c55a4aa5500babd342ad6140b1c98b
M A Anson <ansonantony783@gmail.com>
2026-02-09 17:43:24 +0530

    add visitor model

--- visitors/models.py
+++ visitors/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Visitor(models.Model):
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=15)
+    purpose = models.CharField(max_length=250)
+    check_in_time = models.DateTimeField(auto_now_add=True)
+    check_out_time = models.DateTimeField(null = True, blank=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.phone}"
